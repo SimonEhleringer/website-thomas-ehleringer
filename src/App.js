e6bc524309434cf05e41cf20d029ebfc2a77136f
Simon Ehleringer <simon@ehleringer.de>
2020-12-17 20:33:26 +0100

    Replace BrowserRouter with HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Index from './pages/index';
 import Navbar from './parts/Navbar';
 import ScrollToTopButton from './components/ScrollToTopButton';
